8265224fe22f2b71cb3ad5a8f999727168ff9655
Shreeraksha M S <shreeraksha.ms@ibm.com>
2025-08-09 22:44:21 +0530

    Added dockerfile

--- main1.py
+++ main1.py
@@ -1,5 +1,10 @@
 import os
+from fastapi import FastAPI
+
+app = FastAPI()
+@app.get("/")
+def read_root():
+    test = os.getenv('test_value',"Default Value")
+    return {"test_value": test}
 
 
-test = os.getenv('test_value',"Default Value")
-print(f"Test Value: {test}")
